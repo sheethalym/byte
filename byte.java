f579a2d8cba55c19011587695e20aaac073609ac
sheethalym <sheethal.xworkz@gmail.com>
2021-12-20 21:36:42 +0530

    adding byte program

---
+++ byte.java
@@ -0,0 +1,17 @@
+class Byte{
+  public static void main(String[]args){
+  byte []id={19,6,20,12,15,2,7,8,5,10,18};
+   System.out.println(id[0]);
+   System.out.println(id[1]);
+   System.out.println(id[2]);
+   System.out.println(id[3]);
+   System.out.println(id[4]);
+   System.out.println(id[5]);
+   System.out.println(id[6]);
+   System.out.println(id[7]);
+   System.out.println(id[8]);
+   System.out.println(id[9]);
+   System.out.println(id[10]);
+ }
+
+}
